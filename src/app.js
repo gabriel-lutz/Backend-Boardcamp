91845d1d01fb9ed27c2a3e4012653cb1f8d8c716
Gabriel Peres Lutz <gabriel.p.lutz@gmail.com>
2021-06-16 18:34:56 -0300

    adicionada rota get para /categories

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
-import express from "express"
-import cors from "cors"
-import pg from "pg"
+import express from "express";
+import cors from "cors";
+import pg from "pg";
 
 const app = express();
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+
+const {Pool} = pg;
+const connection  = new Pool(
+    {
+        user: 'bootcamp_role',
+        password: 'senha_super_hiper_ultra_secreta_do_role_do_bootcamp',
+        host: 'localhost',
+        port: 5432,
+        database: 'boardcamp'
+    }
+);
+
+app.get('/categories', async (req,res)=>{
+    try{
+        const query = await connection.query('SELECT * FROM categories')
+        res.send(query.rows)
+    }catch(err){
+        consolçe.log(err)
+        res.send(500)
+    }
+})
 
 app.listen(4000, ()=>{
     console.log("O servidor está rodando na porta 4000...")
-})
+});
